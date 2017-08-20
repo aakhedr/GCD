597742870dae97b526da4f1feb892e1366e24ef7
Ahmed Khedr <ahmed.a.khedr@outlook.com>
2017-08-20 13:31:06 +0200

    Add comments

--- gcd.py
+++ gcd.py
@@ -1,6 +1,7 @@
 def naive_gcd(a, b):
 	"""
-	
+	a, b = Integers
+	Find greatest common divisor the easy naive way.	
 	"""
 	best = 0
 	for d in range(1, max([a, b])):
@@ -10,7 +11,8 @@ def naive_gcd(a, b):
 
 def eucledian_gcd(a, b):
 	"""
-
+	a, b = Integers
+	Use Eucledian method to find GCD where GCD(a, b) = GCD(b, a`) 
 	"""
 	if b == 0:
 		return a
